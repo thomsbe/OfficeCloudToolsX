406812685ea1fad69e144b900f4ac335eaecc9c7
Thomas Baer <thomas.baer@tbaer.eu>
2016-04-04 16:42:55 +0200

    Kaffeekasse Datenstruktur

--- kaffeekasse/models.py
+++ kaffeekasse/models.py
@@ -1,2 +1,30 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
+from django.db import models
+
+from officecloud.models import OfficeUser
+
+
+class Attachment(models.Model):
+    attachment_name = models.CharField('Name of the attachment', max_length=255)
+    attachment_filename = models.CharField('Path of the attachment', max_length=255)
+    attachment_date = models.DateField()
+
+
+class Purchasing(models.Model):
+    purchasing_name = models.CharField(max_length=100)
+    purchasing_date = models.DateField()
+    purchasing_value = models.FloatField()
+
+
+class DebtStatus(models.Model):
+    status_name = models.CharField(max_length=20)
+    status_desc = models.CharField(max_length=255)
+
+
+class Debt(models.Model):
+    debt_creditor = models.ForeignKey(OfficeUser, related_name='debitor')
+    debt_payer = models.ForeignKey(OfficeUser, related_name='creditor')
+    debt_value = models.FloatField('Value of this debt')
+    debt_status = models.ForeignKey(DebtStatus)
