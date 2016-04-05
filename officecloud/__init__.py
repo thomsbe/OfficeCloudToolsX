825126b6091fc433fc656fbf34bc78126ce08335
Martin Blatter <martin.blatter@solongo.de>
2016-04-05 13:59:18 +0200

    Widgetcontent für officecloud main module

--- officecloud/__init__.py
+++ officecloud/__init__.py
@@ -1,3 +1,5 @@
+from django.utils.html import format_html
+
 IS_OFFICE_CLOUD = True
 
 def get_name():
@@ -7,4 +9,5 @@ def get_link():
     return "index"
 
 def get_welcome_widget():
-    return ""
+    html = '<h4 class="center-align"><i class="mdi mdi-action-home"></i>&nbsp;'+get_name()+'</h4>'
+    return format_html(html)
